1991e5abe55dfc06196131923958173ad115376d
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2022-09-21 18:44:36 +0200

    fix: 404 page

--- app.js
+++ app.js
@@ -18,6 +18,23 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 
+// 404 Page Not Found
+app.use((req, res) =>
+  res.status(404).json({
+    message: 'Oops! You are lost, Go to home page 🌏',
+    statusCode: 404
+  })
+);
+
+// 500 Internal Server Error
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    message: 'Oops! Something went wrong, Try again later',
+    statusCode: 500
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- routes/index.js
+++ routes/index.js
@@ -42,21 +42,4 @@ router.get('/:formId', async (req, res, next) => {
   }
 });
 
-// 404 Page Not Found
-router.use((req, res) =>
-  res.status(404).json({
-    message: 'Oops! You are lost, Go to home page 🌏',
-    statusCode: 404
-  })
-);
-
-// 500 Internal Server Error
-router.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({
-    message: 'Oops! Something went wrong, Try again later',
-    statusCode: 500
-  });
-});
-
 export default router;
